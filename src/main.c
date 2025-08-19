1b5cf001f72999eeb55f678878f5b3f0411fc861
raven0034 <monkeyboy2805@gmail.com>
2025-08-19 14:53:11 +0100

    Refactor fds

--- src/main.c
+++ src/main.c
@@ -11,14 +11,14 @@
 #include "ctmp.h"
 
 int main() {
-    int proxy_fd, src_fd;
-    struct sockaddr_in proxy_addr;
+    int src_listen_fd, src_client_fd;
+    struct sockaddr_in src_listen_addr;
     const int opt = 1;
 
     // AF_INET = IPv4
     // SOCK_STREAM = TCP
     // 0 = IPPROTO_TCP (implied)
-    if ((proxy_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((src_listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         // any negative int = error/unexpected behaviour
         perror("socket failed");
         exit(EXIT_FAILURE);
@@ -26,23 +26,23 @@ int main() {
 
     // allow reuse of socket
     // not secure in prod, TIME_WAIT should be respected
-    if (setsockopt(proxy_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+    if (setsockopt(src_listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
-    proxy_addr.sin_family = AF_INET;
-    proxy_addr.sin_addr.s_addr = INADDR_ANY; // all interfaces
-    proxy_addr.sin_port = htons(PORT); // convert to network byte order
+    src_listen_addr.sin_family = AF_INET;
+    src_listen_addr.sin_addr.s_addr = INADDR_ANY; // all interfaces
+    src_listen_addr.sin_port = htons(PORT); // convert to network byte order
 
     // bind to addr and port
-    if (bind(proxy_fd, (struct sockaddr *) &proxy_addr, sizeof(proxy_addr)) < 0) {
+    if (bind(src_listen_fd, (struct sockaddr *) &src_listen_addr, sizeof(src_listen_addr)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
     // passive, wait for src, max 1 src
-    if (listen(proxy_fd, 1) < 0) {
+    if (listen(src_listen_fd, 1) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
@@ -52,7 +52,7 @@ int main() {
 
     // accept incoming
     // blocking until connect
-    if ((src_fd = accept(proxy_fd, NULL, NULL)) < 0) {
+    if ((src_client_fd = accept(src_listen_fd, NULL, NULL)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
@@ -62,7 +62,7 @@ int main() {
     ctmp_header header;
     // read
     //ssize_t bytes_read = read(src_fd, buffer, BUFFER_SIZE);
-    ssize_t bytes_read = read(src_fd, &header, sizeof(ctmp_header));   // for robustness, need to loop read realistically, for now just drop if we don't get sufficient bytes for simplicity
+    ssize_t bytes_read = read(src_client_fd, &header, sizeof(ctmp_header));   // for robustness, need to loop read realistically, for now just drop if we don't get sufficient bytes for simplicity
     if (bytes_read > 0) {
         if (bytes_read != sizeof(ctmp_header)) {
             perror("header read failed (insufficient bytes)");
@@ -78,7 +78,7 @@ int main() {
 
         char buffer[header.length + 1]; // +1 to allow null terminator for now to avoid unsafety with string funcs
 
-        ssize_t data_read =  read(src_fd, &buffer, header.length);
+        ssize_t data_read =  read(src_client_fd, &buffer, header.length);
 
         buffer[data_read] = '\0';
         printf("Read from source (%zd) bytes: %s\n", data_read, buffer);
@@ -89,8 +89,8 @@ int main() {
     }
 
     // close connections
-    close(src_fd);
-    close(proxy_fd);
+    close(src_client_fd);
+    close(src_listen_fd);
 
     printf("Proxy exiting...\n");
     return 0;
